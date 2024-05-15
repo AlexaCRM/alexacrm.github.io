6d8dcc729c6ec7f5ded2faa148ea8cd91b38bd84
Yuliya Pazniak <yuliya@alexacrm.com>
2024-05-15 21:38:35 +0300

    remove tagline

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -4,7 +4,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
   title: 'Datapress',
-  tagline: 'Dinosaurs are cool',
+  tagline: '',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
