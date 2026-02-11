78594792456623d54c97fc4624a7264e487a8d6b
Yuliya Pazniak <yuliya@alexacrm.com>
2026-02-11 19:17:14 +0300

    change twig redirect

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -77,7 +77,7 @@ const config: Config = {
             from: ['/integration-cds/views'],
           },
           {
-            to: '/twig/introduction', 
+            to: '/twig/overview/', 
             from: ['/integration-cds/twig'],
           },
           {
